258364924997c873dcc31263ac849f09a55656a9
alsdncka <alsdncka@naver.com>
2018-06-20 15:23:55 +0900

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import {MainComponent} from  './main/main.component';
+import { ErrorPageComponent } from './error-page/error-page.component';
+import {HeaderComponent} from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+const appRoutes: Routes = [
+  { path: '', component: MainComponent },
+  { path: '',
+    redirectTo: '/main',
+    pathMatch: 'full'
+  },
+  { path: '**', component: ErrorPageComponent }
+];
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ErrorPageComponent,
+    MainComponent,
+    HeaderComponent,
+    FooterComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule, RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true }),
   ],
   providers: [],
   bootstrap: [AppComponent]
